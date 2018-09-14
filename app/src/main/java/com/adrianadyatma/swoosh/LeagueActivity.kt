05721337e3b12b6410d2d52cf3958bffc24732fe
Adrian-Ryzen <adrianadyatma21@gmail.com>
2018-09-14 17:02:49 +0700

    Made 3rd Activity's layout

--- app/src/main/java/com/adrianadyatma/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/adrianadyatma/swoosh/LeagueActivity.kt
@@ -1,12 +1,26 @@
 package com.adrianadyatma.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+//        button2.setOnClickListener {
+//            val i = Intent(this, SkillActivity::class.java)
+//            startActivity(i)
+//        }
+    }
+
+    fun goToSkillActivity(view: View){
+
+        val i = Intent(this, SkillActivity::class.java)
+        startActivity(i)
     }
 }
