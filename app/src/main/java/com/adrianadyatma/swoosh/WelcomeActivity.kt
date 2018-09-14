4cc4dbb6db51ecfc62c540e08506f04190e4a470
Adrian-Ryzen <adrianadyatma21@gmail.com>
2018-09-14 16:19:41 +0700

    2nd lesson complete
    Making 2nd activity's layout

--- app/src/main/java/com/adrianadyatma/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/adrianadyatma/swoosh/WelcomeActivity.kt
@@ -1,12 +1,21 @@
 package com.adrianadyatma.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        button.setOnClickListener {
+            val i = Intent(this, LeagueActivity::class.java)
+            startActivity(i)
+        }
     }
 }
